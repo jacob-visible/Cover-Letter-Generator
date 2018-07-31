b1b6f91042518c0a7daef59080cd9f50209e4402
six-sigma-fu <41940500+six-sigma-fu@users.noreply.github.com>
2018-07-31 06:12:50 -0400

    Add files via upload

---
+++ Cover Letter Generator/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace CoverLetterGenerator
+{
+    partial class CGen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.DebugBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.DebugBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Requirements";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 225);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cover Letter";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.Location = new System.Drawing.Point(7, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(500, 170);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsReturn = true;
+            this.textBox2.Location = new System.Drawing.Point(7, 241);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(729, 330);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TabStop = false;
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(7, 199);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(100, 23);
+            this.GenerateButton.TabIndex = 3;
+            this.GenerateButton.Text = "Generate";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(513, 23);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(224, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(510, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Job Title (optional)";
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.Location = new System.Drawing.Point(113, 199);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(100, 23);
+            this.CopyButton.TabIndex = 9;
+            this.CopyButton.TabStop = false;
+            this.CopyButton.Text = "Copy to Clipboard";
+            this.CopyButton.UseVisualStyleBackColor = true;
+            this.CopyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(219, 199);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(100, 23);
+            this.ClearButton.TabIndex = 5;
+            this.ClearButton.TabStop = false;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(513, 66);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(224, 20);
+            this.textBox4.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(513, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Addressing (optional)";
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.Location = new System.Drawing.Point(110, 225);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(0, 13);
+            this.errorLabel.TabIndex = 14;
+            // 
+            // DebugBox
+            // 
+            this.DebugBox.Location = new System.Drawing.Point(513, 173);
+            this.DebugBox.Name = "DebugBox";
+            this.DebugBox.Size = new System.Drawing.Size(221, 20);
+            this.DebugBox.TabIndex = 15;
+            this.DebugBox.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(510, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Debug Box";
+            this.label5.Visible = false;
+            // 
+            // DebugBox2
+            // 
+            this.DebugBox2.Location = new System.Drawing.Point(513, 215);
+            this.DebugBox2.Name = "DebugBox2";
+            this.DebugBox2.Size = new System.Drawing.Size(221, 20);
+            this.DebugBox2.TabIndex = 17;
+            this.DebugBox2.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(513, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Debug Box 2";
+            this.label6.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(513, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Change Requirement Keywords";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(513, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(223, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Change Output to Textbox";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // CGen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(748, 576);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.DebugBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.DebugBox);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.GenerateButton);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CGen";
+            this.RightToLeftLayout = true;
+            this.Text = "Cover Letter Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button CopyButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label errorLabel;
+        private System.Windows.Forms.TextBox DebugBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox DebugBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Cover Letter Generator/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoverLetterGenerator
+{
+    public partial class CGen : Form
+    {
+        public CGen()
+        {
+            InitializeComponent();
+        }
+
+        //Path to Directory Variable
+        public class Config
+        {
+            public static string Path = Environment.GetFolderPath(Environment.SpecialFolder.Personal) + @"\CoverLetterGenerator\";//@"C:\Users\*USERNAME*\My Documents\"
+        }
+
+            //Start of generate button press function
+            private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = String.Empty;
+            string[] qual = textBox1.Lines;
+
+            //Load Config Replace Line Settings
+            //Load Config for keywords or qualifications
+            if (File.Exists(Config.Path + @"qual.txt"))
+            {
+                ;
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory(Config.Path);
+                System.IO.File.WriteAllText(Config.Path + @"qual.txt", @"each line is a seperate keyword that this program will search for
+Add keywords for requirements that will be give qualifications on the output
+The correspond text that will output to the textbox will be on the same line on the require.txt doc");
+            }
+
+            //Load Config for Output text or requirement
+            if (File.Exists(Config.Path + @"require.txt"))
+            {
+                ;
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory(Config.Path);
+                System.IO.File.WriteAllText(Config.Path + @"require.txt", @"each line is a seperate output
+Add qualifications that will output when the keyword on the same line in the qual.txt is
+The corresponding text will output in the textbox");
+            }
+
+
+            //Load Text to Variables
+            string[] RequireText = File.ReadAllLines(Config.Path + @"require.txt");
+            string[] QualText = File.ReadAllLines(Config.Path + @"qual.txt");
+
+
+
+
+            //Header Name, Info
+            textBox2.Text += @"Your Name
+Phone: Number
+Email: Address
+Date: ";
+            textBox2.Text += DateTime.Now.ToString("dd/MM/yyyy")
+            + @"
+
+";
+            if (textBox4.TextLength > 0)
+            {
+                textBox2.Text += @"Dear " + textBox4.Text + @",";
+            }
+            else
+            {
+                textBox2.Text += @"To whom it may concern,";
+            }
+            textBox2.Text += @"
+            Moving from the west coast to Montreal has been exciting. Your recent posting for ";
+
+            if (textBox3.TextLength > 0)
+            {
+                if (textBox3.Text.StartsWith( "Assistant" ) || textBox3.Text.StartsWith("Administrative") || textBox3.Text.StartsWith("Admissions"))
+                {
+                    textBox2.Text += "an ";
+                } else
+                {
+                    textBox2.Text += "a ";
+                }
+                textBox2.Text += textBox3.Text;
+            }
+            else
+            {
+                textBox2.Text += @"a job";
+            };
+
+            textBox2.Text += @" caught my attention as I am highly interested in pursuing a career here. Currently, I am able to start immediately. Given my work experience, related knowledge, and excellent capabilities; I would make a good fit for this opening.
+";
+if (textBox1.TextLength > 0)
+            {
+
+                //Requirements section
+                textBox2.Text += @"
+Your Requirements
+";
+                int q = 0;
+                for (q = 0; q < qual.Length; q++)
+                {
+                    if (qual[q].StartsWith("    "))
+                    {
+                        textBox2.Text += "- " + qual[q].Substring(4);
+                    }
+                    else
+                    {
+                        if (qual[q].StartsWith("   "))
+                        {
+                            textBox2.Text += "- " + qual[q].Substring(3);
+                        }
+                        else
+                        {
+                            if (qual[q].StartsWith("  ") || qual[q].StartsWith("? ") || qual[q].StartsWith("· ") || qual[q].StartsWith("- ") || qual[q].StartsWith("• "))
+                            {
+                                textBox2.Text += "- " + qual[q].Substring(2);
+                            }
+                            else
+                            {
+                                if (qual[q].StartsWith(" ") || qual[q].StartsWith("·") || qual[q].StartsWith("-") || qual[q].StartsWith("•") || qual[q].StartsWith("?"))
+                                {
+                                    textBox2.Text += "- " + qual[q].Substring(1);
+                                }
+                                else
+                                {
+                                    textBox2.Text += "- " + qual[q];
+                                }
+                            }
+                        }
+                    }
+                    textBox2.Text += @"
+";
+                } 
+            }
+            else
+            {
+                ;
+            }
+
+            //Qualifications section
+            textBox2.Text += @"
+My Qualifications
+";
+            int QualCount = 0;
+            bool hit = false;
+            string RequireTextMain = "";
+            string RequireTextRemainder = "";
+            //replace requirement lines with new qualification lines
+            for (int qualline = 0; qualline < qual.Length; qualline++)
+            {
+                for (int requireline = 0; requireline < RequireText.Length; requireline++)
+                {
+                    if (RequireText[requireline].Length <= 0)
+                    {
+                        ;
+                    }
+                    else
+                    {
+                        //Runs || check for multiple options in requirements
+                        if (RequireText[requireline].Contains("||"))
+                        {
+                            //|| loop for requirements checking individually until || in requirements
+                            RequireTextRemainder = RequireText[requireline];
+                            hit = false;
+                            do
+                            {
+                                RequireTextMain = RequireTextRemainder.Substring(0, RequireText[requireline].IndexOf("||"));
+                                RequireTextRemainder = RequireTextRemainder.Substring(RequireTextRemainder.IndexOf("||") + 2);
+                                if (qual[qualline].Contains(RequireTextMain))
+                                {
+                                    textBox2.Text += @"- ";
+                                    textBox2.Text += QualText[requireline];
+                                    RequireText[requireline] = "";
+                                    QualText[requireline] = "";
+                                    RequireTextRemainder = "";
+                                    hit = true;
+                                    QualCount++;
+                                    break;
+                                }
+                                else
+                                {
+                                    ;
+                                }
+                            }
+                            while (RequireTextRemainder.Contains("||"));
+                            if (hit == true)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                ;
+                            }
+                        }
+                        else
+                        {
+                            if (qual[qualline].Contains(RequireText[requireline]))
+                            {
+                                textBox2.Text += @"- ";
+                                textBox2.Text += QualText[requireline];
+                                RequireText[requireline] = "";
+                                QualText[requireline] = "";
+                                hit = true;
+                                QualCount++;
+                                break;
+                            }
+                            else
+                            {
+                                ;
+                            };
+                        }
+                    }
+                };
+                    textBox2.Text += @"
+";
+            };
+
+            //Final Statement
+            textBox2.Text += @"
+I appreciate you taking the time to review my qualifications, briefly stated here. My Resume shows these further. Please, let me know if you are interested in meeting me in person. Thank you.
+
+Sincerely, 
+Your Name";
+
+            //Auto Copy to Clipboard
+            if (textBox2.TextLength > 0)
+            {
+            //    Clipboard.SetText(textBox2.Text);
+            }
+            else
+            {
+                ;
+            }
+
+            //Error text label
+            if (textBox1.TextLength <= 0)
+            {
+                errorLabel.ForeColor = Color.Red;
+                errorLabel.Text = @"Empty Requirements";
+            }
+            else
+            {
+                if (QualCount < qual.Length)
+                {
+                    errorLabel.ForeColor = Color.Red;
+                    errorLabel.Text = @"Missing Qualification(s) for ";
+                    errorLabel.Text += qual.Length - QualCount;
+                    errorLabel.Text += @" Requirement(s)";
+                }
+                else
+                {
+                    errorLabel.ForeColor = Color.Green;
+                    errorLabel.Text = @"It Worked";
+                }
+            }
+        }
+        //End of generate button press
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            if (textBox2.TextLength > 0)
+            {
+                Clipboard.SetText(textBox2.Text);
+            }
+            else
+            {
+                ;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+            textBox4.Text = null;
+            DebugBox.Text = null;
+            DebugBox2.Text = null;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Load Config for keywords or qualifications
+            if (File.Exists(Config.Path + @"qual.txt"))
+            {
+                ;
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory(Config.Path);
+                System.IO.File.WriteAllText(Config.Path + @"qual.txt", @"each line is a seperate keyword that this program will search for
+Add keywords for requirements that will be give qualifications on the output
+The correspond text that will output to the textbox will be on the same line on the require.txt doc");
+            }
+
+            //Load Config for Output text or requirement
+            if (File.Exists(Config.Path + @"require.txt"))
+            {
+                ;
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory(Config.Path);
+                System.IO.File.WriteAllText(Config.Path + @"require.txt", @"each line is a seperate output
+Add qualifications that will output when the keyword on the same line in the qual.txt is
+The corresponding text will output in the textbox");
+            }
+
+            Process.Start(Config.Path + @"require.txt");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Load Config for keywords or qualifications
+            if (File.Exists(Config.Path + @"qual.txt"))
+            {
+                ;
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory(Config.Path);
+                System.IO.File.WriteAllText(Config.Path + @"qual.txt", @"each line is a seperate keyword that this program will search for
+Add keywords for requirements that will be give qualifications on the output
+The correspond text that will output to the textbox will be on the same line on the require.txt doc");
+            }
+
+            //Load Config for Output text or requirement
+            if (File.Exists(Config.Path + @"require.txt"))
+            {
+                ;
+            }
+            else
+            {
+                System.IO.Directory.CreateDirectory(Config.Path);
+                System.IO.File.WriteAllText(Config.Path + @"require.txt", @"each line is a seperate output
+Add qualifications that will output when the keyword on the same line in the qual.txt is
+The corresponding text will output in the textbox");
+            }
+
+            Process.Start(Config.Path + @"qual.txt");
+        }
+    }
+}
